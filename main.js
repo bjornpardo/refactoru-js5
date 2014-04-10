7c940573e39a66a5e13e0f09ea770c6bad462c3d
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-10 17:28:38 -0600

    apr10528

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 $(document).on('ready', function() {
+
+	$('.form').hide(); 
+	$('.editName').on('click', function(){
+  	$(this).hide();
+  	// $('.form').show();
+  	$('.editName').after('<input></input>');
+  	});
+  	
+  	$('input').keyup(function() {
+    var value = $(this).val();
+    });
+
+  	$('.editLoc').on('click', function(){
+  	$(this).hide();
+  	// $('.form').show();
+  	$('.editLoc').after('<textarea></textarea>');
+  	});
+
+
+  	$('.LocOk').on('click', function(){
+  	$('<textarea></textarea>').val();
+
+  	});
   
 });
 
-$('p').editInPlace({
-    url: "http://com.examplesite.www/users",
-    params: "name=david"
-});
+
+    
